301b864c1f89f0ba38de2e8fcdd742a49fb2a217
Scott Harrison <scottrules44@gmail.com>
2022-09-11 19:13:52 -0400

    Update to adapter for Max 11.4

--- plugins/2018.3326/iphone-sim/metadata.lua
+++ plugins/2018.3326/iphone-sim/metadata.lua
@@ -3,8 +3,8 @@ local metadata =
 	plugin =
 	{
 		format = 'staticLibrary',
-		staticLibs = {  },
-		frameworks = { "AppLovinMediationIronSourceAdapter", "IronSource" },
+		staticLibs = { "IronSourceAdapter", "IronSource" },
+		frameworks = {  },
 		frameworksOptional = {},
 	},
 }
